67e15361d5418791ccfbb86bef2c015a611809fd
Satyam <satyamjha1607@gmail.com>
2023-12-06 16:42:09 +0530

    landing page uploaded

---
+++ js.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const parallaxContainer = document.querySelector(".parallax-container");
+
+//   window.addEventListener("scroll", function () {
+//     const scrollPosition = window.scrollY;
+
+//     // Adjust the threshold as needed to trigger the effect at the desired scroll position
+//     const threshold = 300;
+
+//     if (scrollPosition > threshold) {
+//       parallaxContainer.classList.add("active");
+//     } else {
+//       parallaxContainer.classList.remove("active");
+//     }
+//   });
+// });
